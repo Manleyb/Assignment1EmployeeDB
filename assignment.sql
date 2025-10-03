b4419754110600c96c4f9579e6d65cba671d19ec
bManley <brycemanleyy@gmail.com>
2025-10-03 11:57:24 -0700

    Create assignment.sql
    itit commit

---
+++ assignment.sql
@@ -0,0 +1,46 @@
+-- Assignment: Restaurant Database
+-- Team Members: Bryce
+-- This file will contain all the SQL queries for the assignment.
+
+-- 2. Create a database called restaurant.
+CREATE DATABASE restaurant;
+
+-- 1. Model Orders, Customers and Dishes for a restaurant.
+-- 3. Create tables - Orders, Customers and Dishes
+
+
+
+-- 4. Add 10 rows of data to each table.
+
+
+
+-- 5. Write a query to select orders for a given customer.
+
+
+
+-- 6. write a query to find all the dishes a customer has had.
+
+
+
+-- 7. Write a query to find the total amount a customer has to pay for an order.
+
+
+
+-- 8. Write a query to find the total amount a customer has spent in the restaurant
+
+
+
+-- 9. What is the most popular dish in the restaurant
+
+
+
+-- 10. Create a role + user restaurant_owner who can do any thing in the restaurant db
+
+
+
+-- 11. create a role + user for servers who can add orders
+
+
+
+-- 12. create a role + user for cooks who can add dishes
+
